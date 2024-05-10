99b3a2ac609eeb0edebe19d2802c81a39a672707
x6styghu <js1achidian@hotmail.com>
2024-05-11 04:26:06 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-function buildTree(preorder, inorder) {
-  if (preorder.length === 0 || inorder.length === 0) return null;
-  const rootVal = preorder[0];
-  const root = new TreeNode(rootVal);
-  const index = inorder.indexOf(rootVal);
-  const leftInorder = inorder.slice(0, index);
-  const rightInorder = inorder.slice(index + 1);
-  const leftPreorder = preorder.slice(1, 1 + leftInorder.length);
-  const rightPreorder = preorder.slice(1 + leftInorder.length);
-  root.left = buildTree(leftPreorder, leftInorder);
-  root.right = buildTree(rightPreorder, rightInorder);
-  return root;
+function kthSmallest(matrix, k) {
+  const flatten = matrix.reduce((acc, row) => acc.concat(row), []);
+  flatten.sort((a, b) => a - b);
+  return flatten[k - 1];
 }
