72b2c92e549a5338ef4d3268fe21ba5c60462cd0
x6styghu <js1achidian@hotmail.com>
2024-05-12 11:23:58 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-function isSubsequence(s, t) {
-  let i = 0;
-  let j = 0;
-  while (i < s.length && j < t.length) {
-    if (s[i] === t[j]) i++;
-    j++;
+function maxSlidingWindow(nums, k) {
+  const result = [];
+  const queue = [];
+  for (let i = 0; i < nums.length; i++) {
+    while (queue.length && nums[i] >= nums[queue[queue.length - 1]]) {
+      queue.pop();
+    }
+    queue.push(i);
+    if (queue[0] === i - k) queue.shift();
+    if (i >= k - 1) result.push(nums[queue[0]]);
   }
-  return i === s.length;
+  return result;
 }
